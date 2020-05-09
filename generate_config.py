07baba270f5c766baddb9a075f5e9a3b4f3b0efe
HSSLC <62177829+HSSLC@users.noreply.github.com>
2020-05-09 08:32:35 +0800

    add "direction" in automatic generated config

--- generate_config.py
+++ generate_config.py
@@ -4,6 +4,7 @@ def generate_config(title, creator, language='zh', folder='jpg', width=800, heig
     config = {}
     config['title'] = title
     config['creator'] = creator
+    config['direction'] = 'rl'
     config['language'] = language
     config['folder'] = folder
     config['width'] = width
